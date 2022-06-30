507fa7a8cbc8ffdc026587ed82d20795db2ed688
Derek Mendez <dermen@lbl.gov>
2022-06-30 10:58:15 -0700

    udpates, prevents opening too many files

--- eiger/write_virtual_eiger.py
+++ eiger/write_virtual_eiger.py
@@ -1,7 +1,6 @@
 
 import os
 import h5py
-
 import numpy as np
 import glob
 
@@ -28,46 +27,47 @@ NSPOTS_POS = 1
 
 def get_frames_per_h5(master_file, verbose=False):
     master_dir = os.path.dirname(master_file)
-    h = h5py.File(master_file, "r")
-    img_grp = h['entry/data']
-    external_links = list(img_grp.keys())
-    if verbose:
-        print(external_links)
-    linked_files = []
-    for l in external_links:
-        link = img_grp.get(l, getlink=True)
-        link_filename = os.path.join(master_dir, link.filename)
-        if os.path.exists(link_filename):
-            file_num = int(link_filename.split("_")[-1].split(".")[0])
-            linked_files.append((file_num, link_filename, link.path))
+    with h5py.File(master_file, "r") as h:
+        img_grp = h[MASTER_ENTRY]
+        external_links = list(img_grp.keys())
+        if verbose:
+            print(external_links)
+        linked_files = []
+        for l in external_links:
+            link = img_grp.get(l, getlink=True)
+            link_filename = os.path.join(master_dir, link.filename)
+            if os.path.exists(link_filename):
+                file_num = int(link_filename.split("_")[-1].split(".")[0])
+                linked_files.append((file_num, link_filename, link.path))
 
-    if not linked_files:
-        raise OSError("no linked files found in master file %s" % master_file)
-    linked_files = sorted(linked_files)
-    _,first_file, dset_path = linked_files[0]
-    first_h = h5py.File(first_file, "r")
-    nframes = first_h[dset_path].shape[0]
+        if not linked_files:
+            raise OSError("no linked files found in master file %s" % master_file)
+        linked_files = sorted(linked_files)
+        _,first_file, dset_path = linked_files[0]
+        first_h = h5py.File(first_file, "r")
+        nframes = first_h[dset_path].shape[0]
 
-    nzeros = len(external_links[0].split("_")[1])
-    
+        nzeros = len(external_links[0].split("_")[1])
+        
     return nframes, nzeros
 
 
 
 def get_dset_sample(master_file):
     master_dir = os.path.dirname(master_file)
-    h = h5py.File(master_file, "r")
-    img_grp = h[MASTER_ENTRY]
-    external_links = list(img_grp.keys())
-    for l in external_links:
-        try:
-            dset = img_grp[l]
-            break
-        except KeyError:
-            dset = None
-            pass
-    if dset is None:
-        raise OSError("hdf5 master file %s has no readable external links" % master_file)
+    with h5py.File(master_file, "r") as h:
+        img_grp = h[MASTER_ENTRY]
+        external_links = list(img_grp.keys())
+        for l in external_links:
+            try:
+                dset = img_grp[l]
+                break
+            except KeyError:
+                dset = None
+                pass
+        if dset is None:
+            raise OSError("hdf5 master file %s has no readable external links" % master_file)
+
     return dset
 
 
@@ -79,25 +79,25 @@ def dataDir_to_virtual(dat_dir, out_name):
     fnames = glob.glob("%s/*master*" % dat_dir)
     all_entries = []
     for i_f, f in enumerate(fnames):
-        h = h5py.File(f, 'r')
-        G = h['entry/data']
-        nfiles =G.keys()
-        nfiles = sorted(nfiles, key=lambda x: int(x.split("_")[-1]))
-        offset = 0
-        entries =[]
-        for k in nfiles:
-            link = G.get(k, getlink=True)
-            
-            filepath = os.path.join(os.path.dirname(f), link.filename)
-            if not os.path.exists(filepath):
-                continue
-            dset = h5py.File(filepath, 'r')[link.path]
-            print(os.path.basename(f), os.path.basename(filepath), dset.shape[0])
-            n = dset.shape[0]
-            global_inds = np.arange(offset, offset+n,1)
-            entries += list(zip([os.path.basename(f)]*n, global_inds))
-            offset += n
-        all_entries+= entries
+        with h5py.File(f, 'r') as h:
+            G = h[MASTER_ENTRY]
+            nfiles =G.keys()
+            nfiles = sorted(nfiles, key=lambda x: int(x.split("_")[-1]))
+            offset = 0
+            entries =[]
+            for k in nfiles:
+                link = G.get(k, getlink=True)
+                
+                filepath = os.path.join(os.path.dirname(f), link.filename)
+                if not os.path.exists(filepath):
+                    continue
+                dset = h5py.File(filepath, 'r')[link.path]
+                print(os.path.basename(f), os.path.basename(filepath), dset.shape[0])
+                n = dset.shape[0]
+                global_inds = np.arange(offset, offset+n,1)
+                entries += list(zip([os.path.basename(f)]*n, global_inds))
+                offset += n
+            all_entries+= entries
 
     fname_info = []
     for entry in all_entries:
@@ -120,13 +120,14 @@ def scoreFile_to_virtual(score_file, min_score, out_name, min_nspots=10):
     header = lines[:2]
     data_dir = header[0].split("{")[1].split()[4]
 
-
     entries = lines[2:]
-        
 
     entries = np.array([l.strip().split() for l in entries])
-    scores = entries[:,SCORE_POS].astype(int)  # TODO: update with real Jinhu file
-    nspots = entries[:,NSPOTS_POS].astype(int)  # TODO: update with real Jinhu file
+    if len(entries)==0:
+        print("Not entries in file!")
+        return
+    scores = entries[:,SCORE_POS].astype(int)
+    nspots = entries[:,NSPOTS_POS].astype(int)
     # choose the good ones
     sel = (scores >= min_score ) * ( nspots >= min_nspots)
     if not np.any(sel):
@@ -135,10 +136,6 @@ def scoreFile_to_virtual(score_file, min_score, out_name, min_nspots=10):
 
     scores = scores[sel]
     nspots = nspots[sel]
-    # optionally sort by score ? 
-    #order = np.argsort(scores)[::-1]
-    #scores = scores[order]
-    #fname_info = entries[sel, 0][order]
 
     fname_info = entries[sel, 0]
 
@@ -165,13 +162,27 @@ def main(fname_info, data_dir, out_name, zero_based_indexing=True):
     unique_masters = set(all_master_fnames)
     print("getting frames per master file (%d master files)" % len(unique_masters))
 
-    frame_per_master = { name: get_frames_per_h5(name) for name in unique_masters }
+    frame_per_master = {}
+    for i_name, name in enumerate(unique_masters):
+        frame_per_master[name] = get_frames_per_h5(name)
+        print(i_name, len(unique_masters))
 
     num_imgs = len(all_dset_inds)
 
     open_masters = {}
     for f in unique_masters:
-        open_masters[f] = h5py.File(f, 'r')[MASTER_ENTRY]
+        try:
+            open_masters[f] = h5py.File(f, 'r')[MASTER_ENTRY]
+        except OSError as err:
+            err_s = str(err)
+            if "Too many open files" in err_s:
+                keys = list(open_masters)
+                # remove a few keys  so we can still open more hdf5s later...
+                for k in keys[:10]:
+                    open_masters.pop(k)
+                break
+            else:
+                raise OSError(err_s)
 
     test_dset = get_dset_sample(all_master_fnames[0])
     sdim = test_dset.shape[1]
@@ -189,7 +200,10 @@ def main(fname_info, data_dir, out_name, zero_based_indexing=True):
         master_file_idx = int(dset_idx / nframes)
         master_file_offset = dset_idx % nframes
 
-        data_grp = open_masters[master_name]
+        if master_name in open_masters:
+            data_grp = open_masters[master_name]
+        else:
+            data_grp = h5py.File(master_name, 'r')[MASTER_ENTRY]
 
         link_name = "data_%s"
         link_name %= str(master_file_idx+1).zfill(nzeros)
@@ -201,15 +215,12 @@ def main(fname_info, data_dir, out_name, zero_based_indexing=True):
         source_key= master_name, source_path
 
         if source_key not in sources:
-            source_shape = open_masters[master_name][link_name].shape
+            source_shape = data_grp[link_name].shape
             assert master_file_offset < source_shape[0], "offset is supposed to be within dset bounds! (offset=%d, source_sh0=%d)" % (master_file_offset, source_shape[0])
             Vsource = h5py.VirtualSource(master_name, source_path, shape=source_shape)
             sources[source_key] = Vsource
 
-        try:
-            SOURCE = sources[source_key][master_file_offset]
-        except:
-            from IPython import embed;embed()
+        SOURCE = sources[source_key][master_file_offset]
 
         Vlayout[i_img] = SOURCE #$sources[source_key][master_file_offset] 
 
